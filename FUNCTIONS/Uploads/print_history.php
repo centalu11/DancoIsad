1558eb1cb5944e5a88c461a927aabb06b88b6d49
Nico <trueshot18@gmail.com>
2019-11-12 22:15:59 +0800

    11/12/2019 10:15PM

--- FUNCTIONS/Uploads/print_history.php
+++ FUNCTIONS/Uploads/print_history.php
@@ -10,11 +10,11 @@ foreach($_POST as $k=>$v){
 
 
 $class = new Admin($data);
-$data = $class->get_history_logs($data);
+$data = $class->get_accounts_logs($data);
 
 class PDF extends FPDF
 {
-	function construct ($margin = 5) 
+	function construct ($margin = 10) 
 	{ 
 		$this->SetTopMargin($margin); 
 		$this->SetLeftMargin($margin); 
@@ -26,7 +26,7 @@ class PDF extends FPDF
 	{
 	    $this->SetFont('Arial', 'B', 10); 
 		$this->SetFillColor(36, 96, 84); 
-		$this->Cell(0, 0, 'Activity Logs', 0, 0, 'C'); 
+		$this->Cell(0, 0, 'History Logs', 0, 0, 'C'); 
 	    $this->Ln(5);
 	}
 
@@ -38,19 +38,18 @@ class PDF extends FPDF
 	$pdf = new PDF();
 		// $pdf-$column_widths = ['50','50','50','50'];
 		$pdf->construct();
-		$pdf->AddPage('L','Legal'); 
-		$pdf->Cell(40, 10, 'Name' , 'LTBR', 0, 'L'); 
-		$pdf->Cell(70, 10, 'Committed by' , 'LTBR', 0, 'L'); 
-		$pdf->Cell(80, 10, 'Committed Date' , 'LTBR', 0, 'L'); 
-		$pdf->Cell(150, 10, 'Action' , 'LTBR', 0, 'L'); 
+		$pdf->AddPage('P','Legal'); 
+		$pdf->Cell(70, 10, 'ACTION' , 'LTBR', 0, 'C'); 
+		$pdf->Cell(70, 10, 'Committed by' , 'LTBR', 0, 'C'); 
+		$pdf->Cell(55, 10, 'Committed Date' , 'LTBR', 0, 'C'); 
+		//$pdf->Cell(150, 10, 'Action' , 'LTBR', 0, 'L'); 
 		$pdf->Ln();
 		foreach ($data['result'] as $k => $v) { 
 		$pdf->SetFont('Arial', 'B', 10); 
 		$pdf->SetFillColor(36, 96, 84);
-		$pdf->Cell(40, 10, $v['name'], 'LTBR', 0, 'L');
-		$pdf->Cell(70, 10, $v['first_name'].' '.$v['last_name'], 'LTBR', 0, 'L');
-		$pdf->Cell(80, 10, $v['date_created'], 'LTBR', 0, 'L');
-		$pdf->Cell(150, 10, $v['action'], 'LTBR', 0, 'L');
+		$pdf->Cell(70, 10, $v['action'], 'LTBR', 0, 'C');
+		$pdf->Cell(70, 10, $v['name'], 'LTBR', 0, 'C');
+		$pdf->Cell(55, 10, $v['date_created'], 'LTBR', 0, 'C');
 		$pdf->Ln();
 			}
 		
